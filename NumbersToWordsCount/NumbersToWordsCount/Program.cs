1f765aff6e19029255921501ecca9572bbc90b58
ColinBloemendaal-COAR <colin.bloemendaal@gmail.com>
2021-12-16 10:30:19 +0100

    Made the algoritm work with numbers up to 20

--- NumbersToWordsCount/NumbersToWordsCount/Program.cs
+++ NumbersToWordsCount/NumbersToWordsCount/Program.cs
@@ -10,6 +10,100 @@ namespace NumbersToWordsCount
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the value of which you want to start from (> 0 && < 21): ");
+            string sta = Console.ReadLine();
+            Console.WriteLine("Enter the value of which you want the program to stop at (> 0 && < 21): ");
+            string end = Console.ReadLine();
+            Int32.TryParse(sta, out int st);
+            Int32.TryParse(end, out int en);
+            int TotalCount = TotalCharCount(st, en);
+            Console.WriteLine("The total count of characters from the letter " + st + " to " + en + " is: " + TotalCount);
+
+
+            // 132 = honderd en twee en dertig
+
+
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+        }
+
+
+ 
+
+        public static int TotalCharCount(int st, int en)
+        {
+            int count = 0;
+            for(int i = st; i <= en; i++)
+            {
+                count += GetCharCount(Get1to20(i));
+            }
+            return count;
+        }
+
+        public static int GetCharCount(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+                return 0;
+            int count = 0;
+            foreach(char c in s)
+            {
+                count++;
+            }
+            return count;
+        }
+        
+        public static string Get1to20(int num)
+        {
+            if(num > 0 && num <= 20)
+            {
+                switch (num)
+                {
+                    case 1:
+                        return "Een";
+                    case 2:
+                        return "Twee";
+                    case 3:
+                        return "Drie";
+                    case 4:
+                        return "Vier";
+                    case 5:
+                        return "Vijf";
+                    case 6:
+                        return "Zes";
+                    case 7:
+                        return "Zeven";
+                    case 8:
+                        return "Acht";
+                    case 9:
+                        return "Negen";
+                    case 10:
+                        return "Tien";
+                    case 11:
+                        return "Elf";
+                    case 12:
+                        return "Twaalf";
+                    case 13:
+                        return "Dertien";
+                    case 14:
+                        return "Viertien";
+                    case 15:
+                        return "Vijftien";
+                    case 16:
+                        return "Zestien";
+                    case 17:
+                        return "Zeventien";
+                    case 18:
+                        return "Achtien";
+                    case 19:
+                        return "Negentien";
+                    case 20:
+                        return "Twintig";
+                    default:
+                        return "";
+                }
+            }
+            return "";
         }
     }
 }
---
+++ NumbersToWordsCount/NumbersToWordsCount/README.md
@@ -0,0 +1,50 @@
+﻿1-10
+11-20
+een-en-twintig
+100-en-een-en-twintig
+elf-honderd-een-en-twintig 1121
+duizend-honder-en-een 1101
+tienduizend-honderd-en-een 101001
+
+1	een
+2	twee
+10	tien
+11	elf
+12	twaalf
+20	twintig
+21	een-en-twintig
+22	twee-en-twintig
+100	honderd
+101 honderd-en-een
+111 honderd-en-elf
+112 honderd-en-twaalf
+999 negen-honderd-negen-en-negentig
+
+1->20 uitschrijven
+switch/case
+
+Methode / Array
+loop 1-10
+woorden uitschrijven
+
+Logica voor alles tot 20
+Handmatig uitschrijven
+
+Alles > 20-99
+Aantal getallen tellen
+2 getallen -> 10 tal, en laatste getal omdraaien
+- Methode aanroepen om 1-10 converteren
+- Plak 10-tal en omgedraaide getal aan elkaar. Strip spaties en -
+Tel aantal karakters.
+Geef resultaat
+
+Alles > 100
+
+Tel aantal karakters
+
+101
+111
+1111
+3 getallen -> honderd
+4 getallen -> duizend
+5 getallen -? tienduizend
