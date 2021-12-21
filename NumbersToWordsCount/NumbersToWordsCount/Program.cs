b2f6c8369c418e1c0e029fa58a4a44ebab666b4f
ColinBloemendaal-COAR <colin.bloemendaal@gmail.com>
2021-12-21 13:09:29 +0100

    Made the application more user friendly

--- NumbersToWordsCount/NumbersToWordsCount/Program.cs
+++ NumbersToWordsCount/NumbersToWordsCount/Program.cs
@@ -15,33 +15,86 @@ namespace NumbersToWordsCount
             bool Application = true;
             while (Application)
             {
-                Console.WriteLine("Enter the value of which you want to start from (> 0 && < 21): ");
-                string sta = Console.ReadLine();
-                Console.WriteLine("Enter the value of which you want the program to stop at (> 0 && < 21): ");
-                string end = Console.ReadLine();
-                Int32.TryParse(sta, out int st);
-                Int32.TryParse(end, out int en);
-
-                int TotalCount = TotalCharCount(st, en);
-                Console.WriteLine("The total count of characters from the letter " + st + " to " + en + " is: " + TotalCount);
-
-                Console.Write("Press <Escape> to exit or press <Enter> to start over.");
-                var key = Console.ReadKey().Key;
-                if (key == ConsoleKey.Escape)
+                string sta = String.Empty;
+                string end = String.Empty;
+                while(CheckInput(sta) == false)
                 {
-                    Application = false;
-                    Environment.Exit(0);
+                    Console.WriteLine("Enter the value of which you want to start from (larger then 0): ");
+                    sta = Console.ReadLine();
                 }
-                if (key == ConsoleKey.Enter)
+
+                while (CheckInput(end) == false)
                 {
-                    Console.Clear();
-                    continue;
+                    Console.WriteLine("Enter the value of which you want the program to stop at (larger then 0): ");
+                    end = Console.ReadLine();
                 }
+
+                ReturnResult(sta, end);
+
+
+                Application = ExitProgram();
             }
         }
 
-        public static int TotalCharCount(int st, int en)
+        public static bool CheckInput(string s)
         {
+            if(!string.IsNullOrEmpty(s) && IsInt(s) && GetInt(s) > 0)
+                return true;
+            return false;
+
+        }
+
+        public static void ReturnResult(string sta, string end )
+        {
+            if (GetInt(sta) > GetInt(end))
+            {
+                Console.WriteLine("The starting value must be lower then the ending value");
+                return;
+            }
+            int TotalCount = TotalCharCount(sta, end);
+            Console.WriteLine("The total count of characters from the letter " + sta + " to " + end + " is: " + TotalCount);
+        }
+
+        public static bool IsInt(string s)
+        {
+            if(Int32.TryParse(s, out int i))
+                return true;
+            return false;
+        }
+
+        public static int GetInt(string s)
+        {
+            Int32.TryParse(s, out int i);
+            return i;
+        }
+
+        public static bool ExitProgram()
+        {
+            Console.Write("Press <Escape> to exit or press <Enter> to start over.");
+
+            ConsoleKeyInfo key;
+            do
+            {
+                key = Console.ReadKey(true);
+            } while (key.Key != ConsoleKey.Enter && key.Key != ConsoleKey.Escape);
+
+            if (key.Key == ConsoleKey.Escape)
+            {
+                Environment.Exit(0);
+                return false;
+            }
+            if (key.Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                return true;
+            }
+            return false;
+        }
+
+        public static int TotalCharCount(string s, string e)
+        {
+            int st = GetInt(s);
+            int en = GetInt(e);
             int count = 0;
             for(int i = st; i <= en; i++)
             {
@@ -53,7 +106,7 @@ namespace NumbersToWordsCount
         // Split the original number in its own parts: 932 = 900, 30, 2
         public static string SplitNumber(int num)
         {
-            if (num < 0)
+            if (num <= 0)
                 return "";
 
             string numW = num.ToString();
@@ -76,15 +129,17 @@ namespace NumbersToWordsCount
                 lastTwo = Convert.ToInt32(array[array.Length - 2].ToString() + array[array.Length - 1].ToString());
 
             int TensOfThousands = 0;
-            if(numLength >= 5)
+            if(numLength >= 5 && num >= 10000)
                 TensOfThousands = Convert.ToInt32(array[array.Length - 5].ToString() + array[array.Length - 4].ToString());
 
+            
             if (CheckUnique(lastTwo) == true)
             {
                 if(numLength >= 2)
                     array[array.Length - 2] = 0;
                 array[array.Length - 1] = Convert.ToInt32(lastTwo);
             }
+
             // Reverse the array so the index is in a corresponding position with the math function to the power of.
             // Array index 2 is in math to the power of 2 > num * (10 * the power of 2)
             Array.Reverse(array);
@@ -188,8 +243,7 @@ namespace NumbersToWordsCount
                     case 80000: return "Tachtigduizend";
                     case 90000: return "Negentigduizend";
                     case 100000:return "HonderdDuizend";
-                    case 1000000:
-                        return "Miljoen";
+                    case 1000000: return "Miljoen";
                     default:
                         return "";
                 }
